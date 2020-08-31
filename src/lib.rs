ae475509b735255b8d405d97d1ea81099bfd0c11
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-08-31 20:37:51 +0700

    Add decodeJwt function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
 use wasm_bindgen::prelude::*;
+use base64;
+use std::str;
 
 #[wasm_bindgen]
 pub fn say(s: &str) -> String {
   println!("The Rust function say() received {}", s);
   let r = String::from("hello ");
   return r + s;
+}
+
+#[wasm_bindgen]
+pub fn decodeJwt(token: &str) -> String {
+    let split_encoded_string = token.split(".");
+    let vec_splitted_string = split_encoded_string.collect::<Vec<_>>();
+    let decode_base64 = base64::decode(vec_splitted_string[1]).unwrap();
+    let decoded_string = String::from(str::from_utf8(&decode_base64).unwrap());
+
+    return decoded_string;
 }
